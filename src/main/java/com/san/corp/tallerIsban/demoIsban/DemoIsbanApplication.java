78afa0cbd6ad0acd9f2f372410537ff54ab6eea8
arodriro <arodrirov@everis.com>
2019-11-11 16:58:42 +0100

    commit inicial

---
+++ src/main/java/com/san/corp/tallerIsban/demoIsban/DemoIsbanApplication.java
@@ -0,0 +1,13 @@
+package com.san.corp.tallerIsban.demoIsban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIsbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIsbanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/san/corp/tallerIsban/demoIsban/DemoIsbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.corp.tallerIsban.demoIsban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIsbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
